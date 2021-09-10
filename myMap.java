053ca4cecd870d37c3e8d210e85d3ef316395c2c
johnxiler <65880846+johnxiler@users.noreply.github.com>
2021-09-10 17:39:16 +0800

    Add files via upload

---
+++ myMap.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class myMap
+{
+    
+  //  public double distance(double time, double speed){
+       
+   // }
+    public static void main(String[] args){
+       String[] mapLocation={}; 
+        
+    }
+}
